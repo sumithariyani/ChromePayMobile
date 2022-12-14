6c68582346c66a579f11b8f79309373db6a9d614
sumithariyani <sumit.hariyani1@gmail.com>
2022-12-14 17:24:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chromepaymobile/LinkServicesActivity.java
@@ -0,0 +1,73 @@
+package com.example.chromepaymobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.chromepaymobile.Api.Network.AllUrl;
+
+public class LinkServicesActivity extends AppCompatActivity {
+
+    TextView fuseId;
+    ImageView backImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link_services);
+
+        fuseId = findViewById(R.id.fuse_id);
+        backImage = findViewById(R.id.back_img_linkServices);
+
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("CustomerPreferences",MODE_PRIVATE);
+        String id = sharedPreferences.getString("walletAddress","");
+
+        fuseId.setText(id);
+    }
+
+    public void LinkedApi(){
+
+        try {
+            SharedPreferences sharedPreferences = getSharedPreferences("CustomerPreferences",MODE_PRIVATE);
+            String token = sharedPreferences.getString("token","");
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, AllUrl.ORGLinked + token, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+            requestQueue.add(stringRequest);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
